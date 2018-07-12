2944b676a06ec35f43e047fa119c93a1d84068b2
rickyc106 <rickyc106@gmail.com>
2018-07-12 18:10:01 -0500

    Finish vector math. Need visualization

--- src/arduino/motor_testing.ino
+++ src/arduino/motor_testing.ino
@@ -4,17 +4,19 @@
 #include <std_msgs/Float32.h>
 #include <std_msgs/Float32MultiArray.h>
 //#include <moving_base/arduino_status.h>
+#include <tf/tf.h>
 
 ros::NodeHandle nh;
 
-double steer_angle;
-double x, y;
-double steer_mag;
+float x, y;
+float steer_angle;
+float steer_mag;
 
-double rotate_angle[4];
-double rotate_mag[4];
+float rotate_angle[4];
+float rotate_mag[4];
 
-double res_x, res_y;
+float res_x, res_y;
+float resultant_angle[4];
 float resultant[4];
 
 float error;
@@ -29,30 +31,11 @@ bool forwards[4];
 
 std_msgs::Float32 temp;
 std_msgs::Float32MultiArray temp_array;
+//std_msgs::Float32MultiArray array_angle;
 //moving_base::arduino_status stats;
 
 void xbox_cb(const sensor_msgs::Joy& msg) {
   steer_angle = atan2(msg.axes[1], -msg.axes[0]);
-
-  // DO ALL TRIG MATH HERE ------------------------------------------------------------
-
-  x = cos(steer_angle) * -msg.axes[0];
-  y = sin(steer_angle) * msg.axes[1];
-
-  steer_mag = sqrt(x*x + y*y);
-
-  for (int i = 0; i < 4; i++) {
-    rotate_mag[i] = 1.0;
-    resultant[i] = vector_sum(steer_angle, steer_mag, rotate_angle[i], rotate_mag[i]);
-  }
-
-  // FINISH TRIG MATH -----------------------------------------------------------------
-
-  steer_angle *= (180 / 3.141592653);
-
-  temp_array.data = resultant;
-  temp.data = resultant[0];
-  //stats.steer_angle = angle;
 
   if (msg.buttons[4] && !msg.buttons[5]) {
     CCW = true;
@@ -63,11 +46,49 @@ void xbox_cb(const sensor_msgs::Joy& msg) {
     no_rotate = false;
   }
   if (!msg.buttons[4] && !msg.buttons[5]) no_rotate = true;
+
+  // DO ALL TRIG MATH HERE ------------------------------------------------------------
+
+  x = cos(steer_angle) * -msg.axes[0];
+  y = sin(steer_angle) * msg.axes[1];
+
+  steer_mag = sqrt(x * x + y * y);
+
+  for (int i = 0; i < 4; i++) {
+    if (CCW && !no_rotate) {
+      rotate_angle[i] += M_PI / 180;
+      rotate_mag[i] = 1.0;
+    }
+    else if (!CCW && !no_rotate) {
+      rotate_angle[i] -= M_PI / 180;
+      rotate_mag[i] = 1.0;
+    }
+
+    if (i == 0 || i == 1) rotate_mag[i] = -1.0;
+
+    if (no_rotate) {
+      rotate_mag[i] = 0.0;
+    }
+
+    if (steer_mag != 0.0 || rotate_mag[i] != 0.0) {
+      resultant[i] = vector_sum(steer_angle, steer_mag, rotate_angle[i], rotate_mag[i], i);
+    }
+  }
+
+  // FINISH TRIG MATH -----------------------------------------------------------------
+
+  steer_angle *= (180 / 3.141592653);
+
+  temp_array.data = resultant_angle;
+  //array_angle.data = rotate_angle;
+  temp.data = rotate_angle[0] * (180 / M_PI);
+  //stats.steer_angle = angle;
 }
 
 ros::Subscriber < sensor_msgs::Joy > joy_sub("joy_queued", &xbox_cb);
 ros::Publisher temp_pub("temp_data", &temp);
 ros::Publisher temp_array_pub("temp_array_data", &temp_array);
+//ros::Publisher array_angle_pub("array_angle", &array_angle);
 //ros::Publisher status_pub("arduino_status", &stats);
 
 void setup() {
@@ -75,18 +96,20 @@ void setup() {
 
   initialize();
   nh.initNode();
-  
+
   nh.subscribe(joy_sub);
   nh.advertise(temp_pub);
   nh.advertise(temp_array_pub);
+  //nh.advertise(array_angle_pub);
   //nh.advertise(status_pub);
 }
 
 void loop() {
   temp_pub.publish(&temp);
   temp_array_pub.publish(&temp_array);
+  //array_angle_pub.publish(&array_angle);
   //status_pub.publish(&stats);
-  
+
   nh.spinOnce();
   //delay(100);
 }
@@ -103,11 +126,11 @@ void PID(float setpoint, float current_angle) {
 }
 
 /*
-void initialize() {
+  void initialize() {
   for (int i = 0; i < 4; i++) {
-    PID(0, stats.steer_motor[i]);    // Set all steer motors back to zero position 
+    PID(0, stats.steer_motor[i]);    // Set all steer motors back to zero position
   }
-  
+
   stats.steer_motor[0] -= 45;
   stats.steer_motor[1] += 45;
   stats.steer_motor[2] -= 45;
@@ -123,31 +146,39 @@ void initialize() {
     forwards[0] = true;
     forwards[1] = true;
     forwards[2] = false;
-    forwards[3] = false;  
+    forwards[3] = false;
   }
-}
+  }
 */
 
 void initialize() {
-  temp_array.layout.dim = (std_msgs::MultiArrayDimension *)
-  malloc(sizeof(std_msgs::MultiArrayDimension) * 2);
-  temp_array.layout.dim[0].size = 4;
-  temp_array.layout.dim[0].stride = 1*4;
-  temp_array.layout.data_offset = 0;
-  temp_array.data = (float *)malloc(sizeof(float)*4);
-  temp_array.layout.dim_length = 1;
+  /*
+    temp_array.layout.dim = (std_msgs::MultiArrayDimension *)
+    malloc(sizeof(std_msgs::MultiArrayDimension) * 2);
+    temp_array.layout.dim[0].size = 4;
+    temp_array.layout.dim[0].stride = 1*4;
+    temp_array.layout.data_offset = 0;
+    temp_array.data = (float *)malloc(sizeof(float)*4);
+    temp_array.layout.dim_length = 1;
+    temp_array.data_length = 4;
+  */
+
   temp_array.data_length = 4;
-  
-  rotate_angle[0] -= 45;
-  rotate_angle[1] += 45;
-  rotate_angle[2] -= 45;
-  rotate_angle[3] += 45;
+  //array_angle.data_length = 4;
+
+  rotate_angle[0] -= M_PI / 4;
+  rotate_angle[1] += M_PI / 4;
+  rotate_angle[2] -= M_PI / 4;
+  rotate_angle[3] += M_PI / 4;
 }
 
-float vector_sum(float steer_angle, float steer_mag, float rotate_angle, float rotate_mag) {
+float vector_sum(float steer_angle, float steer_mag, float rotate_angle, float rotate_mag, int counter) {
   res_x = (steer_mag * cos(steer_angle)) + (rotate_mag * cos(rotate_angle));
   res_y = (steer_mag * sin(steer_angle)) + (rotate_mag * sin(rotate_angle));
 
-  return sqrt(res_x*res_x + res_y*res_y);
+  resultant_angle[counter] = atan2(res_y, res_x);
+  resultant_angle[counter] *= 180 / M_PI;
+
+  return sqrt(res_x * res_x + res_y * res_y);
 }
 
--- src/queue_reducer.py
+++ src/queue_reducer.py
@@ -1,25 +1,51 @@
 import rospy
+import tf
+from geometry_msgs.msg import PoseArray
 from sensor_msgs.msg import Joy
+from std_msgs.msg import Float32MultiArray
 
 #queue = None
 joy_msg = Joy()
+pose_array = PoseArray()
+
+resultant = [0,0,0,0]
+pose_zero = [[0 for i in range(4)] for i in range(4)]
+quat = []
 
 def joy_cb(data):
 	joy_msg.axes = data.axes
 	joy_msg.buttons = data.buttons
 	print str(joy_msg.axes)
 
+def array_cb(msg):
+	for i in range(4):
+		resultant[i] = msg.data[i]
+
 def main():
-	queued_joy = rospy.Publisher('joy_queued', Joy)
+	queued_joy = rospy.Publisher('joy_queued', Joy, queue_size = 10)
+	marker = rospy.Publisher('resultant_angle', PoseArray, queue_size = 10)
 
 	rospy.init_node('queue_reducer', anonymous=True)
 
 	rospy.Subscriber('joy',  Joy, joy_cb)
+	rospy.Subscriber('temp_array_data', Float32MultiArray, array_cb)
+
+	for i in range(4):
+		quat.append(tf.transformations.quaternion_from_euler(0,0,resultant[i]))
+		pose_array.poses[i].orientation = quat[i]
+
+	pose_array.pose.position = [-5,5,0]
+	pose_array.pose[1].position = [-5,-5,0]
+	pose_array.pose[2].position = [5,-5,0]
+	pose_array.pose[3].position = [5,5,0]
+
+	pose_array.header.frame_id = 'Resultant_angles'
 
 	rate = rospy.Rate(30)
 
 	while not rospy.is_shutdown():
 		queued_joy.publish(joy_msg)
+		marker.publish(pose_array)
 		rate.sleep()
 		#print "Reducing queue size to", queue
 
